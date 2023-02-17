f10b990371e6d185ea973fc0ebec4e0a0e062e05
algirdas.tamasauskas <algirdas.tamasauskas@visma.com>
2023-02-17 11:04:45 +0200

    Add small wait for fn4 tests

--- packages/packageB/src/lib/fn4.spec.js
+++ packages/packageB/src/lib/fn4.spec.js
@@ -1,7 +1,10 @@
 import { fn4 } from './fn4';
 
+const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 describe('fn4', () => {
   it('should work', async () => {
+    await wait(1500);
     expect(fn4()).toEqual('fn4');
   });
 });
