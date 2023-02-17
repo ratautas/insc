f24a4bdd211f4d3500c2aa8b8215392a2f63d063
algirdas.tamasauskas <algirdas.tamasauskas@visma.com>
2023-02-17 10:54:09 +0200

    Decrease max timeout

--- packages/packageB/src/lib/fn3.spec.js
+++ packages/packageB/src/lib/fn3.spec.js
@@ -5,7 +5,7 @@ const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 describe('fn3', () => {
   it('should work', async () => {
     // Defer to indicate wether the test is running or not
-    await wait(3000);
+    await wait(4000);
     expect(fn3()).toEqual('fn3');
   });
 });
